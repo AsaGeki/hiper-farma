2b6d1ad35bc837d0aea0bb22b9607b8f8a363683
Arthur Gabriel Oliveira de Macedo <arthurgabrielbr10@gmail.com>
2025-10-28 16:33:47 -0300

    config

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_PORT = os.getenv("DB_PORT")
+
+
+def get_connection():
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS,
+            port=DB_PORT
+        )
+        return conn
+    except psycopg2.Error as e:
+        print("❌ Erro ao conectar ao banco:", e)
+        return None

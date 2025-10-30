1a5c1f5ccad876c70f2de96d6a74927e075b5142
Arthur Gabriel Oliveira de Macedo <arthurgabrielbr10@gmail.com>
2025-10-30 19:19:20 -0300

    testar conexões 4.3

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from app.__init__ import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)

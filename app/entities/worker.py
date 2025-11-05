ec1091e149f663beb334e559929689e0d1cc2f44
Arthur Gabriel Oliveira de Macedo <arthurgabrielbr10@gmail.com>
2025-11-05 17:41:26 -0300

    routes update

--- app/entities/worker.py
+++ app/entities/worker.py
@@ -1,4 +1,4 @@
-from config import get_db_connection
+from config import get_db_connection, get_s3_client
 
 def name_bagde(first_name, last_name):
     if ' ' in first_name:
@@ -9,16 +9,42 @@ def name_bagde(first_name, last_name):
         complete_name = f'{first_name} {last_word}'
         return complete_name
     
-def add_role(role):
-    connect = get_db_connection()
-    cursor = connect.cursor()
-    cursor.execute('''
-                   INSERT INTO roles(name)
-                   VALUES ('%s')
-                   ''', role)
-    cursor.close()
-    connect.close()
+
 
 def add_worker():
     db = get_db_connection()
-    s3 = 
+    s3 = get_s3_client()
+    cursor = db.cursor()
+    first_name = request
+    cursor.execute('''
+                   INSERT INTO worker (first_name, last_name, cpf, picture, hiring_date, 
+                   operation, contact, worker_role, gender)
+                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,)
+                   ''', )
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+ s3 = get_s3_client()
+    image_url = s3.generate_presigned_url(
+    'get_object',
+    Params={'Bucket': S3_BUCKET, 'Key':'teste.jpg'},
+    ExpiresIn=3600
+    )
+    return render_template('index.html', worker=worker, image=image_url)
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from config import get_db_connection, get_s3_client, S3_BUCKET
 
 main = Blueprint('main', __name__)
 workers = Blueprint('workers', __name__)
+add_roles = Blueprint('add_roles', __name__)
+add_workers = Blueprint('add_workers', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
-@workers.route('/workers')
+@workers.route('/workers', methods=['GET'])
 def workers_list():
     connect = get_db_connection()
     cursor = connect.cursor()
@@ -19,8 +21,40 @@ def workers_list():
                    JOIN department d ON d.department_id = dw.department_id
                    ''')
     worker_list = cursor.fetchall()
-    connect.close()
     cursor.close()
+    connect.close()
     return render_template('worker_list.html', worker_list = worker_list)
 
+@add_roles.route('/add_role', methods=['POST'])
+def add_role():
+    connect = get_db_connection()
+    cursor = connect.cursor()
+    role = request.form['role'].lower().strip()
+    cursor.execute('''
+                   INSERT INTO roles(name)
+                   VALUES ('%s')
+                   ''', role)
+    cursor.close()
+    connect.close()
+    return redirect('/workers')
+
+@add_workers.route('/add_worker', methods=['POST'])
+def add_worker():
+    db = get_db_connection()
+    s3 = get_s3_client()
+    cursor = db.cursor()
+    first_name = request.form['first_name'].lower().strip()
+    last_name = request.form['last_name'].lower().strip()
+    cpf = request.form['cpf'].lower().strip()
+    picture = request.form['picture'].lower().strip()
+    hiring_date = request.form['hiring_date'].lower().strip()
+    operation = request.form['operation'].lower().strip()
+    contact = request.form['contact'].lower().strip()
+    worker_role = request.form['worker_role'].lower().strip()
+    gender = request.form['gender'].lower().strip()
+    cursor.execute('''
+                   INSERT INTO worker (first_name, last_name, cpf, picture, hiring_date, 
+                   operation, contact, worker_role, gender)
+                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,)
+                   ''', )
 
